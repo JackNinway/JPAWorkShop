2705bea217233bafb8f089127de3fab2b8a3c4c5
Jack Ninway <jack.ninway@gmail.com>
2022-09-14 09:52:18 +0200

    Initial commit

---
+++ src/main/java/com/example/jpaworkshop/JpaWorkShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWorkShopApplication.class, args);
+	}
+
+}
