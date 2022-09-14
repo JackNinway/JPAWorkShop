3f0f63748208f849a4560b583abb40cc2ad71a5a
Jack Ninway <jack.ninway@gmail.com>
2022-09-14 09:53:31 +0200

    part1

--- src/main/java/com/example/jpaworkshop/JpaWorkShopApplication.java
+++ src/main/java/com/example/jpaworkshop/JpaWorkShopApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaWorkShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpaWorkShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWorkShopApplication.class, args);
+    }
 
 }
